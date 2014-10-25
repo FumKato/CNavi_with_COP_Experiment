556dfd8f232bdaf983e0736039d44fd8b7230224
加藤 史也 <FumKATO@katou-no-MacBook-Air.local>
2014-10-25 14:52:24 +0900

    Delete contexts code

--- contexts/student_context.js
+++ contexts/student_context.js
@@ -2,49 +2,32 @@ if(Meteor.isClient){
 	student_context_operations = {
 		// client/views/cnavi_view.js
 		render_header: function(user){
-			return '<div id="header" class="green"><div id="logo">CNavi</div><div id="headerName">Welcome back <b>' + 
-				user.name + '</b></div></div>';
+			//TODO
 		},
 		
 		// client/views/lesson_list_view.js
 		get_lesson_list_item_class: function(lesson){
-			var className = this.proceeds.get_lesson_list_item_class(lesson);
-			var date = new Date();
-			if(lesson.month < date.getMonth() || (lesson.month == date.getMonth() && lesson.date < date.getDate())){
-				className += " overed";
-			}
-			return className;
+			//TODO
 		},
 		
 		// client/views_submission_view.js
 		render_button: function(){
-			return '<input type="button" class="submissionButton" value="Submit" />';
+			//TODO
 		},
 		
 		// client/controllers/templates/lesson_list_controller.js
 		lesson_list_item_clicked: function($this){
-			this.proceeds.lesson_list_item_clicked($this);
-			if(!$this.hasClass('overed')){
-				Meteor.subscribe('questions', Session.get('myself'), $this.attr('id'), function(){
-					cnavi_view.render('submission');
-				});
-			}
+			//TODO
 		},
 		
 		// client/controllers/templates/submission_controller.js
 		back_button_clicked: function(){
-			cnavi_view.render('lessonList');
+			//TODO
 		},
 		
 		// client/controllers/templates/submission_controller.js
 		submission_button_clicked: function(){
-			var answers = new Array();
-			$('.questionAnswerTextForm').each(function(){
-				answers.push($(this).val());
-			});
-			submissions_model.set_answers(Session.get('lesson_id'), Session.get('myself').id, answers);
-			alert('Submision is completed');
-			cnavi_view.render('lessonList');
+			//TODO
 		},
 	};
 
@@ -55,22 +38,12 @@ if(Meteor.isServer){
 	student_context_operations = {
 		// server/controllers/submissions_controller.js
 		set_answers: function(lesson_id, user_id, answers){
-			var user = this.proceeds.set_answers(lesson_id, user_id, answers);
-			submissions_model.set_submissions(user_id, user.name, lesson_id, answers);
+			//TODO
 		},
 		
 		// server/models/questions_model.js
 		get_questions_by_lesson_id: function(user, lesson_id){
-			return Questions.find(
-				{
-					lesson_id: lesson_id
-				},
-				{
-					fields:{
-						answers: 0
-					}
-				}
-			);
+			//TODO
 		}
 	};
 	
